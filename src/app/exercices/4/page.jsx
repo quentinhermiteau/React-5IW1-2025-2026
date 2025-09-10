f300a3c015da23232514b256bc041173e7449691
Quentin Hermiteau <quentinhermiteau@gmail.com>
2025-09-10 17:59:05 +0200

    Correction exo 4

--- src/app/exercices/4/page.jsx
+++ src/app/exercices/4/page.jsx
@@ -1,6 +1,14 @@
 "use client";
 
 export default function Exo4() {
+  const handleChange = (event) => {
+    const value = event.currentTarget.value;
+
+    if (value.length > 10) {
+      alert("limite de 10 caractères dépassée");
+    }
+  };
+
   return (
     <>
       <div id="notice">
@@ -9,13 +17,13 @@ export default function Exo4() {
           la longueur de l'input est vérifiée.
         </p>
         <p>
-          Si l'input dépase les 10 caractères afficher une alert qui indique que
-          la taille maximum est dépassée.
+          Si l'input dépasse les 10 caractères affichés, une alerte qui indique
+          que la taille maximum est dépassée apparait.
         </p>
       </div>
       <section>
         <h1>Character Limit</h1>
-        <input placeholder="Enter some text" />
+        <input placeholder="Enter some text" onChange={handleChange} />
       </section>
     </>
   );
