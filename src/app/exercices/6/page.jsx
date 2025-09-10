77213f90842d3dab93137ecab418b827aae0d818
Quentin Hermiteau <quentinhermiteau@gmail.com>
2025-09-10 18:38:43 +0200

    Correction exo 6

--- src/app/exercices/6/page.jsx
+++ src/app/exercices/6/page.jsx
@@ -1,19 +1,27 @@
 "use client";
 
+import { useState } from "react";
 import "./styles.css";
 
 export default function Exo6({ characterLimit = 20 }) {
-  const inputValue = "";
+  const [inputValue, setInputValue] = useState("");
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    if (e.currentTarget.value.length > characterLimit) {
+      return;
+    }
+
+    setInputValue(e.currentTarget.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // if input length is too long
-    // alert "The input exceeds the character limit. Please shorten your text."
-    // else
-    // alert "Thanks for your submission"
+    if (inputValue.length > characterLimit) {
+      alert("The input exceeds the character limit. Please shorten your text.");
+    } else {
+      alert("Thanks for your submission");
+    }
   };
 
   return (
@@ -37,7 +45,13 @@ export default function Exo6({ characterLimit = 20 }) {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="limited-text-input">Limited Text Input:</label>
-          <span className="no-error">Characters remaining: TODO</span>
+          <span
+            className={
+              characterLimit - inputValue.length > 0 ? "no-error" : "error"
+            }
+          >
+            Characters remaining: {characterLimit - inputValue.length}
+          </span>
         </div>
         <input
           type="text"
