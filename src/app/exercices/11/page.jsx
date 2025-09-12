22a075e3b9fe7179163830259c3329a786f40201
Quentin Hermiteau <quentinhermiteau@gmail.com>
2025-09-12 11:01:37 +0200

    correction exo 11

--- src/app/exercices/11/page.jsx
+++ src/app/exercices/11/page.jsx
@@ -1,24 +1,44 @@
 "use client";
 
-import { useState } from "react";
+import { useReducer } from "react";
 import Slider from "./Slider";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+const initialState = {
+  count: 0,
+  step: 1,
+};
 
-  const handleIncrement = () => setCount(count + step);
-  const handleDecrement = () => setCount(count - step);
-  const handleReset = () => setCount(0);
-  const handleUpdateStep = (step) => setStep(step);
+const reducer = (state, { action, value }) => {
+  switch (action) {
+    case "increment":
+      return { ...state, count: state.count + state.step };
+    case "decrement":
+      return { ...state, count: state.count - state.step };
+    case "reset":
+      return { ...state, count: 0 };
+    case "setStep":
+      return { ...state, step: value };
+  }
+};
+
+export default function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleIncrement = () => dispatch({ action: "increment" });
+  const handleDecrement = () => dispatch({ action: "decrement" });
+  const handleReset = () => dispatch({ action: "reset" });
+  const handleUpdateStep = (step) =>
+    dispatch({ action: "setStep", value: step });
+
+  console.log(state);
 
   return (
     <main>
       <div id="notice">
-        À l'aide de useEffect et setInterval, faire en sorte que le compteur
+        À l'aide de useReducer et setInterval, faire en sorte que le compteur
         s'incrémente automatiquement toutes les secondes.
       </div>
-      <h1>{count}</h1>
+      <h1>{state.count}</h1>
       <div>
         <button onClick={handleDecrement}>-</button>
         <button onClick={handleIncrement}>+</button>
